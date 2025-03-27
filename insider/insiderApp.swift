f07927da94695ccd34eeb2558145f75a611b1dd1
Ramon Ortiz <ramon.o@ciencias.unam.mx>
2025-03-26 22:56:17 -0600

    Initial Commit

---
+++ insider/insiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  insiderApp.swift
+//  insider
+//
+//  Created by José Ramón Ortiz Castañeda on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct insiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
